8d1dfc35fef5b26a713b34648b7a7c657731f046
ZanetaAsare <asarezaneta@gmail.com>
2018-09-18 14:02:21 +0000

    [Adding the almost complete mycat function]

--- Lab 1/mycat.c
+++ Lab 1/mycat.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
-int main(char *i)
+int main()
 {
-	FILE *read;
-	read =fopen(*i,"r");
-	fprintf(read);
+	char filename[31];
+	printf("Please enter the name of the file with the full extension: ");
+	scanf("%s",filename);
+
+	FILE * fp;
+
+	fp = fopen(("%s",filename),"r");
+	char c[100];
+
+	while(!feof(fp)){
+		fgets(c,100,fp);
+		puts(c);
+	}
+
+	fclose(fp);
+	return(0);
 }
+
