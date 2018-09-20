9796a7960c945827861dafcbd1ea9b5a9c315269
ZanetaAsare <asarezaneta@gmail.com>
2018-09-20 10:15:46 +0000

    [Very final edits to the mycat and mygrep files]

--- Lab 1/mycat.c
+++ Lab 1/mycat.c
@@ -5,14 +5,14 @@
 int main(int argc, char *argv[])
 {
 
-        char invalid_chars[] = "!@#$%^&*()`~+= <>?/,;':\[]{}|";
+        char invalid_chars[] = "!@#$%^&*()`~+= <>?/,;':\[]{}|";          //adding an array of possible characters that could cause a file name to be invalid
         int invalid_found = 0;
 
         int i;
         int len = strlen(invalid_chars);
-        for (i = 0; i < len; ++i)
+        for (i = 0; i < len; ++i)     // an iteration through the array of characters
         {
-                if (strchr(argv[1], invalid_chars[i]) != NULL){
+                if (strchr(argv[1], invalid_chars[i]) != NULL){        //checks to see if any of the characters are in the file name given, returns the first occurence of the character
                         invalid_found = 1;
                         break;
                 }
@@ -22,17 +22,17 @@ int main(int argc, char *argv[])
         }
         else{
 	int k;
-	for(k = 1; k < argc; k++){
+	for(k = 1; k < argc; k++){       //an iteration through the array of argument v provided
 		FILE * fp;
-		fp = fopen(argv[k],"r");
+		fp = fopen(argv[k],"r");        //opens file for reading
 
 		char text;
-		while(!feof(fp)){
-			text = fgetc(fp);
+		while(!feof(fp)){		//while the end of the file has not been reached
+			text = fgetc(fp);	//gets the characters from the file which is then printed next
 			printf("%c",text);
 		}
 
-		fclose(fp);
+		fclose(fp);         //file is closed
 	}
 	}
 		return(0);
--- Lab 1/mygrep.c
+++ Lab 1/mygrep.c
@@ -5,15 +5,15 @@
 
 int main(int argc, char *argv[])
 {
-	char invalid_chars[] = "!@#$%^&*()`~+= <>?/,;':\[]{}|";
+	char invalid_chars[] = "!@#$%^&*()`~+= <>?/,;':\[]{}|";			//an array of charcters that could make a file name invalid
 	int invalid_found = 0;
 
 	int i;
 	int len = strlen(invalid_chars);
-	for (i = 0; i < len; ++i)
+	for (i = 0; i < len; ++i)			//an iteration through the array of characters
 	{
-		if (strchr(argv[2], invalid_chars[i]) != NULL){
-			invalid_found = 1;
+		if (strchr(argv[2], invalid_chars[i]) != NULL){       //a check to see if the character is in the filename provided, it returns the first occurence of the character
+			invalid_found = 1;				//if return array is not empty, it shows that there is an invalid character present
 			break;
 		}
 	}
@@ -23,11 +23,11 @@ int main(int argc, char *argv[])
 	else{
 
 	int k;
-	for(k = 2; k < argc; k++){
+	for(k = 2; k < argc; k++){			//an iteration through the array of arguments provided by the user
 
 	        FILE * fp;
 
-        	fp = fopen(argv[k],"r");
+        	fp = fopen(argv[k],"r");		//file is opened for reading
 
         	char word[100];
 		int search;
@@ -36,19 +36,19 @@ int main(int argc, char *argv[])
 	        while(fgets(word,100,fp)){
 
 
-			search = strcasestr(word,argv[1]);
+			search = strcasestr(word,argv[1]);	//while the lines are being read, strcasestr searches for an occurence of the search item, regardless of case.
 
 			if(search==0)
 			{
 			}
 			else
 			{
-				printf("%s\n",word);
+				printf("%s\n",word);		//prints the line if the search item is found on it
 			}
 	}
 
 
-        fclose(fp);
+        fclose(fp);			//file is closed
 
 	}
 	}
